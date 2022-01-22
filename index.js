ae9c304c44e5a598272dc98d392c5bf1b0aa95f8
SeenuAravindRaj <84611330+Mudivira-Digital-Solutions@users.noreply.github.com>
2022-01-22 12:53:55 +0530

    Applied Cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+var cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
 
 app.use('/araosdevsm/login', require('./routes/login'));
 
