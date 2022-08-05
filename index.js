9e5a8fa907c5d3a0f37d399f2241158132aacf07
seenuaravindrajs <seenu.aravindraj@hireright.com>
2022-08-05 12:32:41 +0530

    Update meta data API

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require('express');
-var cors = require('cors');
+const express = require("express");
+var cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/araosdevsm/login', require('./routes/login'));
-app.use('/araosdevsm/gcp-apis', require('./routes/GoogleCloudRoutes/testApi'));
-app.use('/araosdevsm/gcp-apis/timeline-images', require('./routes/GoogleCloudRoutes/getTimelineImg'));
-app.use('/araosdevsm/gcp-apis/post-image', require('./routes/GoogleCloudRoutes/postImage'));
-app.use('/araosdevsm/createaccount', require('./routes/newAccount'));
+app.use("/araosdevsm/login", require("./routes/login"));
+app.use("/araosdevsm/gcp-apis", require("./routes/GoogleCloudRoutes/testApi"));
+app.use(
+  "/araosdevsm/gcp-apis/timeline-images",
+  require("./routes/GoogleCloudRoutes/getTimelineImg")
+);
+app.use(
+  "/araosdevsm/gcp-apis/post-image",
+  require("./routes/GoogleCloudRoutes/postImage")
+);
+app.use("/araosdevsm/createaccount", require("./routes/newAccount"));
+app.use("/araosdevsm/updateImgMetaData", require("./routes/updateImgMetaData"));
 
-app.listen(PORT, ()=>console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 //https://medium.com/@olamilekan001/image-upload-with-google-cloud-storage-and-node-js-a1cf9baa1876
 //https://medium.com/@olamilekan001/image-upload-with-google-cloud-storage-and-node-js-a1cf9baa1876
---
+++ routes/updateImgMetaData.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const mongoDb = require("../common");
+const router = express.Router();
+
+router.post("/:metaDataType/:updatedBy", (req, res) => {
+  const metaDataType = req.params.metaDataType;
+  const postName = req.body.postName;
+  const postedBy = req.body.postedBy;
+  const comment = req.body.comment;
+  const likedFlag = req.body.likedFlag;
+  const updatedBy = req.params.updatedBy;
+  mongoDb.db.users.find((userErr, userDoc) => {
+    if (userErr)
+      res.status(500).json({ message: "ERROR_IN_FINDING_LIKEDBY_USER" });
+    else {
+      if (userDoc.map(({ userName }) => userName).includes(updatedBy)) {
+        mongoDb.db["timeline-images"].find((postErr, postDoc) => {
+          if (postErr)
+            res.status(500).json({ message: "ERROR_IN_FETCHING_POST" });
+          else {
+            const updatedPost = postDoc.find(
+              ({ userName, image }) =>
+                userName === postedBy && image === postName
+            );
+            if (updatedPost) {
+              let updates;
+              switch (metaDataType) {
+                case "updateLike": {
+                  let likes =
+                    likedFlag === "INCREMENT"
+                      ? `${parseInt(updatedPost.likes) + 1}`
+                      : `${parseInt(updatedPost.likes) - 1}`;
+                  let likedBy =
+                    likedFlag === "INCREMENT"
+                      ? [
+                          ...updatedPost.likedBy,
+                          {
+                            user: updatedBy,
+                            likedOn: Math.round(new Date().getTime() / 1000),
+                          },
+                        ]
+                      : updatedPost.likedBy.filter(
+                          ({ user }) => user !== updatedBy
+                        );
+                  updates = {
+                    likes,
+                    likedBy,
+                  };
+                  break;
+                }
+
+                case "updateComment": {
+                  updates = {
+                    commentSection: [
+                      ...updatedPost.commentSection,
+                      {
+                        userName: updatedBy,
+                        comment,
+                        commentedOn: Math.round(new Date().getTime() / 1000),
+                      },
+                    ],
+                  };
+                  break;
+                }
+              }
+              let filter = {
+                userName: updatedPost.userName,
+                image: updatedPost.image,
+              };
+              mongoDb.db["timeline-images"].updateOne(
+                { ...filter },
+                { $set: { ...updates } },
+                (err, response) => {
+                  if (err)
+                    res
+                      .status(500)
+                      .json({ status: "ERROR_IN_UPDATING_METADATA" });
+                  else {
+                    if (response.nModified) {
+                      let status = `${
+                        metaDataType === "updateLike" ? "LIKE_" : "COMMENT_"
+                      }UPDATED`;
+                      res.status(200).json({ status, updates });
+                    }
+                  }
+                }
+              );
+            }
+          }
+        });
+      }
+    }
+  });
+});
+
+module.exports = router;
