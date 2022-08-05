d7d0b6233cc40bca2c2b47acc3d24af3fcef98d4
seenuaravindrajs <seenu.aravindraj@hireright.com>
2022-08-05 22:01:06 +0530

    Committing the bug in adding new user

--- routes/GoogleCloudRoutes/getTimelineImg.js
+++ routes/GoogleCloudRoutes/getTimelineImg.js
@@ -1,31 +1,42 @@
-const express = require('express');
-const mongoDb = require('../../common');
+const express = require("express");
+const mongoDb = require("../../common");
 const router = express.Router();
 
-router.get('/:username', (req, res) => {
-    const username = req.params.username;
-    mongoDb.db.users.find((err, userDoc) => {
-        if(err) res.json({status: 400, error: "Unknown Error"});
-        else {
-            const usersFrnds = [...userDoc.filter(doc=>doc.userName === username)[0].friends, username];
-            if (usersFrnds.length){
-                mongoDb.db['timeline-images'].find((err, timelineDoc) => {
-                    if(err) res.status(400).json({status: 400, error: "Unknown Error"});
-                    else {
-                        const userTimeline = timelineDoc.filter(doc=>usersFrnds.includes(doc.userName));
-                        if (userTimeline.length) {
-                            const timelineResponse = {
-                                timelineImages: userTimeline.sort((usr1, usr2) => usr1.postedOn - usr2.postedOn),
-                                imagePrefixUrl: "https://storage.googleapis.com/araosdev-social-media"
-                            }
-                            res.status(200).json({...timelineResponse});
-                        }
-                        else res.status(404).json({error: "There's no timeline pictures for this user"});
-                    }
-                });
-            }
-        }
-    })
-})
+router.get("/:username", (req, res) => {
+  const username = req.params.username;
+  mongoDb.db.users.find((err, userDoc) => {
+    if (err) res.json({ status: 400, error: "Unknown Error" });
+    else {
+      const usersFrnds = [
+        ...userDoc.filter((doc) => doc.userName === username)[0].friends,
+        username,
+      ];
+      if (usersFrnds.length) {
+        mongoDb.db["timeline-images"].find((err, timelineDoc) => {
+          if (err)
+            res.status(400).json({ status: 400, error: "Unknown Error" });
+          else {
+            const userTimeline = timelineDoc.filter((doc) =>
+              usersFrnds.includes(doc.userName)
+            );
+            if (userTimeline.length) {
+              const timelineResponse = {
+                timelineImages: userTimeline.sort(
+                  (usr1, usr2) => usr1.postedOn - usr2.postedOn
+                ),
+                imagePrefixUrl:
+                  "https://storage.googleapis.com/araosdev-social-media",
+              };
+              res.status(200).json({ ...timelineResponse });
+            } else
+              res
+                .status(404)
+                .json({ error: "There's no timeline pictures for this user" });
+          }
+        });
+      }
+    }
+  });
+});
 
 module.exports = router;
--- routes/newAccount.js
+++ routes/newAccount.js
@@ -1,35 +1,44 @@
-const express = require('express');
-const mongoDb = require('../common');
+const express = require("express");
+const mongoDb = require("../common");
 const router = express.Router();
 
-router.post('/', (req, res)=>{
-    const userName =  req.body.username;
-    const email = req.body.email;
-    const phoneNumber = req.body.phonenumber;
-    const password = req.body.password;
+router.post("/", (req, res) => {
+  const userName = req.body.username;
+  const email = req.body.email;
+  const phoneNumber = req.body.phonenumber;
+  const password = req.body.password;
 
-    mongoDb.db.users.find((err,doc)=>{
-        if(err) res.json({status: 400, updated: "FAILED"});
-        if(doc.length > 10) res.json({status: 200, updated: "FAILED", message: "ACCOUNT_LIMIT_EXCEEDED"})
-        else{
-            const obj = {
-                userName,
-                email,
-                phoneNumber,
-                password
-            }
-            if(!doc.map(({email})=>email).includes(email)){
-                mongoDb.db.users.insertOne(obj, (err, response)=>{
-                    if(err) res.json({status: 400, updated: "FAILED"});
-                    if(response) {
-                        res.json({status: 200, updated: "OK"});
-                    }
-                    else res.json({status: 400, updated: "FAILED"});
-                });
-            }
-            else res.json({status: 400, updated: "FAILED", message: "ALREADY_EXISTING_ACCOUNT"});
-        }
-    });
+  mongoDb.db.users.find((err, doc) => {
+    if (err) res.json({ status: 400, updated: "FAILED" });
+    if (doc.length > 10)
+      res.json({
+        status: 200,
+        updated: "FAILED",
+        message: "ACCOUNT_LIMIT_EXCEEDED",
+      });
+    else {
+      const obj = {
+        userName,
+        email,
+        phoneNumber,
+        password,
+        friends: [],
+      };
+      if (!doc.map(({ email }) => email).includes(email)) {
+        mongoDb.db.users.insertOne(obj, (err, response) => {
+          if (err) res.json({ status: 400, updated: "FAILED" });
+          if (response) {
+            res.json({ status: 200, updated: "OK" });
+          } else res.json({ status: 400, updated: "FAILED" });
+        });
+      } else
+        res.json({
+          status: 400,
+          updated: "FAILED",
+          message: "ALREADY_EXISTING_ACCOUNT",
+        });
+    }
+  });
 });
 
 module.exports = router;
